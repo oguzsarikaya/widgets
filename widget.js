3a555cd7e3cfbcc8d6a26e452f97d9b5120b849b
oguzsarikaya <151478834+oguzsarikaya@users.noreply.github.com>
2026-03-21 11:27:49 +0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,11 +1,11 @@
 (function () {
   "use strict";
 
-  const BASE_URL = "https://oguzsarikaya.github.io/widgets";
+  const BASE_URL = "https://oguzsarikaya.github.io/widgets.js";
   const WIDGETS = {
-    "sal-ruyasi-urun-sosyal-kanit":        BASE_URL + "/sal-ruyasi/urun-sayfasi/sosyal-kanit/index.html",
-    "tablolife-urun-sosyal-kanit":         BASE_URL + "/tablolife/urun-sayfasi/sosyal-kanit/index.html",
-    "zuum-urun-sosyal-kanit":              BASE_URL + "/zuum-accessories/urun-sayfasi/sosyal-kanit/index.html",
+    "sal-ruyasi-urun-sosyal-kanit": BASE_URL + "/sal-ruyasi/urun-sayfasi/sosyal-kanit/index.js",
+    "tablolife-urun-sosyal-kanit":  BASE_URL + "/tablolife/urun-sayfasi/sosyal-kanit/index.js",
+    "zuum-urun-sosyal-kanit":       BASE_URL + "/zuum-accessories/urun-sayfasi/sosyal-kanit/index.js",
   };
 
   function getWidgetId() {
@@ -20,39 +20,18 @@
     return null;
   }
 
-  function injectWidget(widgetId) {
-    const widgetUrl = WIDGETS[widgetId];
-    if (!widgetUrl) return;
-    if (document.querySelector('[data-widget-id="' + widgetId + '"]')) return;
-
-    const iframe = document.createElement("iframe");
-    iframe.setAttribute("data-widget-id", widgetId);
-    iframe.src = widgetUrl;
-    iframe.style.cssText = "border:none;width:100%;min-height:0;display:block;overflow:hidden;background:transparent;";
-    iframe.setAttribute("scrolling", "no");
-    iframe.setAttribute("allowtransparency", "true");
-
-    const currentScript = document.currentScript || (function () {
-      const all = document.querySelectorAll("script[src*='widget.js']");
-      return all[all.length - 1];
-    })();
-
-    if (currentScript && currentScript.parentNode) {
-      currentScript.parentNode.insertBefore(iframe, currentScript.nextSibling);
-    } else {
-      document.body.appendChild(iframe);
-    }
-
-    iframe.addEventListener("load", function () {
-      try {
-        const h = iframe.contentDocument.body.scrollHeight;
-        if (h) iframe.style.height = h + "px";
-      } catch (_) {}
-    });
+  function loadWidget(widgetUrl) {
+    const script = document.createElement("script");
+    script.src = widgetUrl;
+    document.head.appendChild(script);
   }
 
-  window.addEventListener("message", function (e) {
-    if (e.data && e.data.type === "widget-resize") {
-      const iframe = document.querySelector('[data-widget-id="' + e.data.id + '"]');
-      if (iframe) iframe.style.height = e.data.height + "px";
+  const id = getWidgetId();
+  if (id && WIDGETS[id]) {
+    if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded", function () { loadWidget(WIDGETS[id]); });
+    } else {
+      loadWidget(WIDGETS[id]);
     }
+  }
+})();
